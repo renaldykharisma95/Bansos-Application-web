668c27eaef63f9f836ee5472565a6b45242d1cbd
renaldykharisma <renaldy@radyalabs.com>
2021-02-21 18:10:12 +0700

    console post form

--- src/components/add-penerima/AddPenerima.js
+++ src/components/add-penerima/AddPenerima.js
@@ -49,7 +49,8 @@ export default function AddPenerima(){
                 rt: values.rt,
                 rw: values.rw,
                 alasan: values.alasan
-            }));    
+            }));
+            console.log('values: ', values);    
             setIsLoad(isLoad => isLoad = false);
             history.push('/bansos-receiver');
             Modal.success({content: 'Add Data Success'});
